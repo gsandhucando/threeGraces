330e0de1b167da3eeb1c2d9f0d72801e04600d8a
gsandhucando <gsandhu916@gmail.com>
2022-06-08 03:33:20 -0700

    lights model and on mouse mover flashlight added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import * as THREE from 'three';
-import React, { Suspense, useRef, useEffect } from "react"
-import { Environment, useGLTF, Html, useProgress, PerspectiveCamera } from "@react-three/drei"
-import { Canvas, useFrame } from '@react-three/fiber'
+import React, { Suspense, useRef, useState, useEffect } from "react"
+import { Environment, useGLTF, Html, useProgress, PerspectiveCamera, OrbitControls } from "@react-three/drei"
+import { Canvas, useFrame, useThree } from '@react-three/fiber'
 import gsap, {Power4} from 'gsap'
+import Model from './Model'
 import './App.css';
 
 // ES6:
@@ -17,13 +18,47 @@ function Loader() {
 
 function ThreeGraces() {
   const { scene } = useGLTF("scene.gltf")
-  // useEffect(() => {            
-  //   var t2 = gsap.timeline();
-  //   t2.to(".model", {opacity: 1, duration: 13, ease: Power4.easeIn});
-  // }, []);
-  return <primitive className='model' style={{opacity: 0}} object={scene} position={[-35, -100, -30]} />
+  console.log(scene)
+
+  return <primitive roughness={0.9} metalness={0.5} color="#474747" className='model' style={{opacity: 0}} object={scene} position={[-33, -105, -10]} />
 }
 
+
+function Rig() {
+  const { camera, mouse } = useThree()
+  const vec = new THREE.Vector3()
+  return useFrame(() => {
+  camera.position.lerp(vec.set(mouse.x * 1, mouse.y * 1, camera.position.z), 0.02)}, []
+  )}
+
+
+  function Dodecahedron() {
+    const { viewport } = useThree()
+    // viewport = canvas in 3d units (meters)
+  
+    const ref = useRef()
+    useFrame(({ mouse }) => {
+      const x = (mouse.x * viewport.width)
+      const y = (mouse.y * viewport.height)
+      // console.log(ref.current.position)
+      ref.current.position.set(-x, -y, 30)
+      // ref.current.rotation.set(-y, x, 30)
+    },[])
+  
+    return (
+      <>
+      <spotLight ref={ref} intensity={0.2} position={[0, 0, 30]} angle={0.15} penumbra={.6} decay={2} castShadow />
+      <pointLight intensity={0.02} position={[0, 0, 0]} angle={0.15} penumbra={.6} decay={2} castShadow />
+      {/* <spotLight intensity={0.2} position={[0, 0, -100]}  angle={10} penumbra={.6} decay={2} castShadow /> */}
+      </>
+    )
+  }
+  function Zoom() {
+    useFrame((state) => {
+      state.camera.position.lerp({ x: 0, y: 0, z: 12 }, 0.005)
+      state.camera.lookAt(0, 0, 0)
+    })
+  }
 
 function App() {
 
@@ -57,13 +92,17 @@ function App() {
       </div>
       <div className="canvasContainer">
         <Canvas className='model' style={{ height: '100vh', background: 'black' }} camera={{ position: [0, 0, 10] }}>
-        {/* <pointLight position={[0, 0, -10]} intensity={.07} /> */}
-        {/* <ambientLight position={[0, 0, 1000]} intensity={.02} /> */}
-        <directionalLight position={[-100, 0, -100]} intensity={.07} />
+        <directionalLight position={[0, 0, -100]} intensity={1} />
+        <pointLight position={[0, 0, -1000]} intensity={.06} />
           <Suspense fallback={<Loader />}>
             <ThreeGraces />
+            {/* <Model position={[0, -6, 0]} rotation={[0, -0.2, 0]} /> */}
             {/* <Environment preset="sunset" background /> */}
           </Suspense>
+          <Dodecahedron />
+          {/* <Zoom /> */}
+          <fog attach="fog" args={['black', 0,-1]} />
+          <Rig />
         </Canvas>
       </div>
     </div>
---
+++ src/Model.js
@@ -0,0 +1,42 @@
+import { useRef } from 'react'
+import { useFrame } from '@react-three/fiber'
+import { useGLTF } from '@react-three/drei'
+import lerp from 'lerp'
+
+export default function Model(props) {
+  const group = useRef()
+  const { nodes } = useGLTF('/scene.gltf')
+  useFrame(({ pointer }) => (group.current.rotation.y = lerp(group.current.rotation.y, pointer.x * (Math.PI / 5), 0.005)))
+  return (
+    <group ref={group} {...props}>
+        {console.log(nodes)}
+      {/* <mesh castShadow receiveShadow geometry={nodes.Node_3.geometry} rotation={[-Math.PI / 2, 0, 0]} scale={[0.2, 0.224, 0.224]} dispose={null}>
+        <meshStandardMaterial roughness={0.9} metalness={0.5} color="#474747" />
+      </mesh> */}
+      <Lights />
+    </group>
+  )
+}
+
+function Lights() {
+  const groupL = useRef()
+  const groupR = useRef()
+  const front = useRef()
+  useFrame(({ pointer }) => {
+    groupL.current.rotation.y = lerp(groupL.current.rotation.y, -pointer.x * (Math.PI / 2), 0.1)
+    groupR.current.rotation.y = lerp(groupR.current.rotation.y, pointer.x * (Math.PI / 2), 0.1)
+    front.current.position.x = lerp(front.current.position.x, pointer.x * 12, 0.05)
+    front.current.position.y = lerp(front.current.position.y, 7 + pointer.y * 4, 0.05)
+  })
+  return (
+    <>
+      <group ref={groupL}>
+        <pointLight position={[0, 7, -15]} distance={15} intensity={10} />
+      </group>
+      <group ref={groupR}>
+        <pointLight position={[0, 7, -15]} distance={15} intensity={10} />
+      </group>
+      <spotLight castShadow ref={front} penumbra={0.75} angle={Math.PI / 4} position={[0, 0, 8]} distance={10} intensity={15} shadow-mapSize={[2048, 2048]} />
+    </>
+  )
+}
