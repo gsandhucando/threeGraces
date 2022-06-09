85ada5bdcd32e939b9b06782cfc3a8c8d2eca164
gsandhucando <gsandhu916@gmail.com>
2022-06-08 22:25:40 -0700

    new model and light added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import * as THREE from 'three';
 import React, { Suspense, useRef, useState, useEffect } from "react"
-import { Environment, useGLTF, Html, useProgress, PerspectiveCamera, OrbitControls } from "@react-three/drei"
+import { Environment, useGLTF, Html, useProgress, PerspectiveCamera, OrbitControls, useHelper } from "@react-three/drei"
 import { Canvas, useFrame, useThree } from '@react-three/fiber'
-import gsap, {Power4} from 'gsap'
+import gsap, { Power4 } from 'gsap'
 import Model from './Model'
 import './App.css';
 
 // ES6:
-import * as dat from 'dat.gui';
+// import * as dat from 'dat.gui';
 
-const gui = new dat.GUI();
+// const gui = new dat.GUI();
 
 function Loader() {
   const { progress } = useProgress()
@@ -17,10 +17,10 @@ function Loader() {
 }
 
 function ThreeGraces() {
-  const { scene } = useGLTF("scene.gltf")
+  const { scene } = useGLTF("threeGraces.glb")
   console.log(scene)
 
-  return <primitive roughness={0.9} metalness={0.5} color="#474747" className='model' style={{opacity: 0}} object={scene} position={[-33, -105, -10]} />
+  return <primitive roughness={0.9} metalness={0.5} color="#474747" className='model' style={{ opacity: 0 }} object={scene} position={[-33, -115, -10]} />
 }
 
 
@@ -28,45 +28,75 @@ function Rig() {
   const { camera, mouse } = useThree()
   const vec = new THREE.Vector3()
   return useFrame(() => {
-  camera.position.lerp(vec.set(mouse.x * 1, mouse.y * 1, camera.position.z), 0.02)}, []
-  )}
+    camera.position.lerp(vec.set(mouse.x * 1, mouse.y * 1, camera.position.z), 0.01)
+  }, []
+  )
+}
 
 
-  function Dodecahedron() {
-    const { viewport } = useThree()
-    // viewport = canvas in 3d units (meters)
-  
-    const ref = useRef()
-    useFrame(({ mouse }) => {
-      const x = (mouse.x * viewport.width)
-      const y = (mouse.y * viewport.height)
-      // console.log(ref.current.position)
-      ref.current.position.set(-x, -y, 30)
-      // ref.current.rotation.set(-y, x, 30)
-    },[])
-  
-    return (
-      <>
-      <spotLight ref={ref} intensity={0.2} position={[0, 0, 30]} angle={0.15} penumbra={.6} decay={2} castShadow />
-      <pointLight intensity={0.02} position={[0, 0, 0]} angle={0.15} penumbra={.6} decay={2} castShadow />
-      {/* <spotLight intensity={0.2} position={[0, 0, -100]}  angle={10} penumbra={.6} decay={2} castShadow /> */}
-      </>
-    )
-  }
-  function Zoom() {
-    useFrame((state) => {
-      state.camera.position.lerp({ x: 0, y: 0, z: 12 }, 0.005)
-      state.camera.lookAt(0, 0, 0)
-    })
-  }
+function Dodecahedron() {
+  const { viewport } = useThree()
+  // viewport = canvas in 3d units (meters)
+
+  const ref = useRef()
+  useFrame(({ mouse }) => {
+    const x = (mouse.x * viewport.width)
+    const y = (mouse.y * viewport.height)
+    // console.log(ref.current.position)
+    ref.current.position.set(-x, -y, 30)
+    // ref.current.rotation.set(-y, x, 30)
+  }, [])
+
+  const light = useRef()
+  useHelper(light, THREE.SpotLightHelper, 'cyan')
+
+  return (
+    <>
+      <spotLight ref={ref} intensity={0.1} position={[0, 0, 30]} angle={0.15} penumbra={.6} decay={2} castShadow />
+      <spotLight ref={light} intensity={1} position={[0, -10, -100]} penumbra={10} decay={2} />
+      {/* <spotLight intensity={100} position={[0, -70, -4000]}  angle={10} penumbra={.6} castShadow /> */}
+    </>
+  )
+}
+
+function LightSection2() {
+  const { viewport } = useThree()
+  // viewport = canvas in 3d units (meters)
+
+  const ref = useRef()
+  useFrame(({ mouse }) => {
+    const x = (mouse.x * viewport.width)
+    const y = (mouse.y * viewport.height)
+    // console.log(ref.current.position)
+    ref.current.position.set(-x, -y, 30)
+    // ref.current.rotation.set(-y, x, 30)
+  }, [])
+
+  const light = useRef()
+  useHelper(light, THREE.SpotLightHelper, 'cyan')
+
+  return (
+    <>
+      <spotLight ref={ref} intensity={0.1} position={[0, 0, 30]} angle={0.15} penumbra={.6} decay={2} castShadow />
+      {/* <spotLight ref={light} intensity={100000} position={[0, -10, -100]} penumbra={10} decay={2} /> */}
+      {/* <spotLight intensity={100} position={[0, -70, -4000]}  angle={10} penumbra={.6} castShadow /> */}
+    </>
+  )
+}
+function Zoom() {
+  useFrame((state) => {
+    state.camera.position.lerp({ x: 0, y: 0, z: 12 }, 0.005)
+    state.camera.lookAt(0, 0, 0)
+  })
+}
 
 function App() {
 
-  useEffect(() => {            
+  useEffect(() => {
     var tl = gsap.timeline();
     var t2 = gsap.timeline();
-    tl.to(".ulContainer", {opacity: 1, duration: 1, ease: Power4.easeIn});
-    t2.to(".model", {opacity: 1, duration: .3, ease: Power4.easeIn});
+    tl.to(".ulContainer", { opacity: 1, duration: 1, ease: Power4.easeIn });
+    t2.to(".model", { opacity: 1, duration: .3, ease: Power4.easeIn });
   }, []);
 
   return (
@@ -91,9 +121,9 @@ function App() {
         </ul>
       </div>
       <div className="canvasContainer">
-        <Canvas className='model' style={{ height: '100vh', background: 'black' }} camera={{ position: [0, 0, 10] }}>
-        <directionalLight position={[0, 0, -100]} intensity={1} />
-        <pointLight position={[0, 0, -1000]} intensity={.06} />
+        <Canvas className='model' style={{ height: '100vh', background: 'black' }} camera={{fov: 55, position: [0, 0, 10] }} pixelRatio={window.devicePixelRatio}>
+          <directionalLight position={[0, 0, -100]} intensity={.6} />
+          {/* <directionalLight position={[0, -20, -600]} intensity={10} /> */}
           <Suspense fallback={<Loader />}>
             <ThreeGraces />
             {/* <Model position={[0, -6, 0]} rotation={[0, -0.2, 0]} /> */}
@@ -101,10 +131,27 @@ function App() {
           </Suspense>
           <Dodecahedron />
           {/* <Zoom /> */}
-          <fog attach="fog" args={['black', 0,-1]} />
+          {/* <fog attach="fog" args={['white', 0, 0]} /> */}
           <Rig />
+          {/* <OrbitControls /> */}
         </Canvas>
       </div>
+      <div className='title'>
+        <h2>The</h2>
+        <h1>Three Graces</h1>
+      </div>
+      {/*
+      <div>
+      <Canvas className='model' style={{ height: '100vh', background: 'black' }} camera={{fov: 55, position: [0, 0, 10] }} pixelRatio={window.devicePixelRatio}>
+          <Suspense fallback={<Loader />}>
+            <ThreeGraces />
+          </Suspense>
+          <LightSection2 />
+          <Rig />
+
+        </Canvas>
+      </div>
+    */}
     </div>
   )
 }
